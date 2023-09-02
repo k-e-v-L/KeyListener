8cc7d533320d204335ec1bcb0d0e3db678c624bf
M Kael <kevliiver@gmail.com>
2023-09-02 19:58:10 +0300

    Now we have a rocket that we can move using w, a, s, d and the background is black.

--- MyFrame.java
+++ MyFrame.java
@@ -8,17 +8,22 @@ import java.awt.event.KeyListener;
 public class MyFrame extends JFrame implements KeyListener {
 
     JLabel label;
+    ImageIcon icon;
 
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(null); //null beacause we are manually moving components within frame using keystrokes
 
+        icon = new ImageIcon("rocket2.png");
+
         label = new JLabel();
         label.setBounds(0,0,100,100);
-        label.setBackground(Color.green);
-        label.setOpaque(true);
+        label.setIcon(icon);
+        //label.setBackground(Color.green);
+        //label.setOpaque(true);
 
+        this.getContentPane().setBackground(Color.BLACK);
         this.add(label);
         this.addKeyListener(this);
         this.setVisible(true);
@@ -30,11 +35,11 @@ public class MyFrame extends JFrame implements KeyListener {
         switch (e.getKeyChar()) {
             case 'a': label.setLocation(label.getX()-1,label.getY());
                 break;
-            case 'w': label.setLocation(label.getX(),label.getY()-1);
+            case 'w': label.setLocation(label.getX(),label.getY()-10);
                 break;
-            case 's': label.setLocation(label.getX(),label.getY()+1);
+            case 's': label.setLocation(label.getX(),label.getY()+10);
                 break;
-            case 'd': label.setLocation(label.getX()+1, label.getY());
+            case 'd': label.setLocation(label.getX()+10, label.getY());
                 break;
         }
 
@@ -43,6 +48,16 @@ public class MyFrame extends JFrame implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         //keyPressed Invoked when a physical key is pressed down. Uses the keyCode, int output
+        switch (e.getKeyCode()) {
+            case 37: label.setLocation(label.getX()-10,label.getY());
+                break;
+            case 38: label.setLocation(label.getX(),label.getY()-10);
+                break;
+            case 39: label.setLocation(label.getX()+10,label.getY());
+                break;
+            case 40: label.setLocation(label.getX(), label.getY()+10);
+                break;
+        }
 
     }
 
